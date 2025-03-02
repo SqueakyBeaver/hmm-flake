ad04ec334fa1a3b0661217f38c60339c72bd3755
Beaverr <beaver@transgender.army>
2025-03-02 11:53:47 -0600

    Works at 8.0.0-beta1

--- flake.nix
+++ flake.nix
@@ -3,13 +3,51 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachSystem [
+      "x86_64-linux"
+      # Nothing else tested
+    ] (
+      system: let
+        pname = "HedgeModManager.UI";
+        version = "8.0.0-beta1";
+        sha256 = "sha256-Aj5hepS6mFcUVhGf9Prjfqd7o6U2lHzwXK33Y3qelgs=";
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+        pkgs = import nixpkgs {
+          inherit system;
+        };
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+        dotnet-sdk = pkgs.dotnetCorePackages.sdk_8_0;
+        dotnet-runtime = pkgs.dotnetCorePackages.runtime_8_0;
+      in {
+        packages.default = pkgs.buildDotnetModule {
+          inherit pname version dotnet-runtime dotnet-sdk;
 
-  };
+          src = pkgs.fetchFromGitHub {
+            owner = "hedge-dev";
+            repo = pname;
+            tag = version;
+            hash = sha256;
+          };
+
+          projectFile = "Source/HedgeModManager.UI/HedgeModManager.UI.csproj";
+          nugetDeps = ./deps.json;
+
+          meta = with pkgs.lib; {
+            homepage = "https://github.com/hedge-dev/HedgeModManager";
+            description = "Multiplatform rewrite of Hedge Mod Manager";
+            license = licenses.mit;
+          };
+        };
+
+        apps.default = flake-utils.lib.mkApp {drv = self.packages.${system}.default;};
+      }
+    );
 }
